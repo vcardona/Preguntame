feccef15075aa00ab655c13467f8cd1cc6b36d8d
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-01-24 10:53:03 -0500

    Initial Commit

---
+++ Preguntame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Preguntame
+//
+//  Created by Victor Hugo on 1/24/19.
+//  Copyright © 2019 Vintage Robot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
